a3a4fd006754d84f754955bfa4dbc1a8ca13ff3b
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2023-08-30 19:30:04 +0530

    profile page progress

--- app/(home)/(tabs)/profile.tsx
+++ app/(home)/(tabs)/profile.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Box } from "@/components/box";
-import { Avatar, Separator, Spacer, YStack } from "tamagui";
-import { Image } from "react-native";
+import { Avatar, XStack, Spacer, YStack } from "tamagui";
 import useTheme from "@/hooks/useTheme";
 import { SecondaryButton } from "@/components/button";
 import { StyledText } from "@/components/styledtext";
+import Community from "../../../components/community/Community";
 
 const ProfileScreen = () => {
   const theme = useTheme();
@@ -12,15 +12,39 @@ const ProfileScreen = () => {
   return (
     <Box>
       <YStack p={10}>
-        <Avatar size={72} circular>
-          {/* <Avatar.Image source={{ uri: "https://picsum.photos/200" }} /> */}
+        <XStack
+          p={10}
+          gap={"$4"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <Avatar size={"$8"} circular>
+            {/* <Avatar.Image source={{ uri: "https://picsum.photos/200" }} /> */}
 
-          <Avatar.Fallback bc={"$gray4"} />
-        </Avatar>
+            <Avatar.Fallback bc={"$gray4"} />
+          </Avatar>
+
+          <XStack gap={"$4"}>
+            <YStack alignItems={"center"}>
+              <StyledText fontWeight={"$bold"}>100</StyledText>
+              <StyledText fontSize={"$sm"}>Discussions</StyledText>
+            </YStack>
+
+            <YStack alignItems={"center"}>
+              <StyledText fontWeight={"$bold"}>100</StyledText>
+              <StyledText fontSize={"$sm"}>Followers</StyledText>
+            </YStack>
+
+            <YStack alignItems={"center"}>
+              <StyledText fontWeight={"$bold"}>100</StyledText>
+              <StyledText fontSize={"$sm"}>Following</StyledText>
+            </YStack>
+          </XStack>
+        </XStack>
 
         <Spacer size={"$2"} />
 
-        <StyledText fontSize={"$lg"}>John Doe | @john_doe</StyledText>
+        <StyledText fontSize={"$lg"}>John Doe</StyledText>
 
         <Spacer size={"$2"} />
 
